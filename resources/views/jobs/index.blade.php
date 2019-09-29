2f7957f0885995d059d8587210dd952b1405a3b2
Wouter Nolten <wouternolten@gmail.com>
2019-09-29 13:37:08 +0200

    Adds index view layout

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h1>Your jobs</h1>
     @foreach($jobs as $job)
-    <div class="card">
+    <div class="card mb--lg">
         <ul class="list-group">
             <li class="list-group-item"><b>{{$job->company->name}}</b> (<a
                     href="{{$job->company->url}}">{{$job->company->url}}</a>)</li>
@@ -12,10 +12,15 @@
             <li class="list-group-item">Year: {{$job->start_date}} - {{$job->end_date ?? 'now'}}</li>
             <li class="list-group-item">City: {{$job->company->city}}</li>
             <li class="list-group-item">Responsibilities: {{$job->responsibilities}}</li>
-            {{-- TODO: WRITE OUT TAGS--}}
-            {{-- <li class="list-group-item">Tags: {{ explode($job->tags->pluck('title'), ',') }}</li> --}}
-
-            @dump(implode(",", $job->tags->pluck('name')))
+            <li class="list-group-item">Tags: {{ implode(",", $job->tags->pluck('name')->toArray()) }}</li>
+            <li class="list-group-item">
+                <a href="/jobs/{{$job->id}}/edit" class="btn btn-primary">Edit</a>
+                <form method="POST" action="/jobs/{{$job->id}}" class="form--inline">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger" value="Delete">
+                </form>
+            </li>
         </ul>
     </div>
     @endforeach
